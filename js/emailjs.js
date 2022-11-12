fb24b12b93020ef716c18256f7c5e7d4c1169881
king alber <king.alber93@gamil.com>
2022-11-12 00:59:02 +0000

    add thanks page

--- js/emailjs.js
+++ js/emailjs.js
@@ -12,8 +12,16 @@
       
          emailjs.sendForm(serviceID, templateID, this)
           .then(() => {
+            //To clear from after submit
+            event.preventDefault();
+            const inputs = document.querySelectorAll('#name, #email, #phonenumber, #age, #exampleFormControlTextarea1');
+            inputs.forEach(input => {
+              input.value = '';
+            });
+            //for thanks page after submit the form
+            window.location.href="thanks.html";
             btn.value = 'Send Email';
-            alert('Sent!');
+         //   alert('Sent!');                *this to show the notification on top of page*
           }, (err) => {
             btn.value = 'Send Email';
             alert(JSON.stringify(err));
